5347cbef6d24b835620c4a6e13b820df1972653f
fmuchembi <fmuchembi@gmail.com>
2025-06-14 00:59:06 +0200

    delete datasetcolum map dictionary and create export

--- src/components/blockly/blocks/loadDataBlocks.js
+++ src/components/blockly/blocks/loadDataBlocks.js
@@ -1,56 +1,7 @@
 import * as Blockly from "blockly";
+import datasetColumnsMap from "../constants/constants";
 
 
-// ---column mapping for R inbuilt datasets--------------//
-const datasetColumnsMap = {
-  iris: [
-    ['Sepal.Length', 'Sepal.Length'],
-    ['Sepal.Width', 'Sepal.Width'],
-    ['Petal.Length', 'Petal.Length'],
-    ['Petal.Width', 'Petal.Width'],
-    ['Species', 'Species']
-  ],
-  mtcars: [
-    ['mpg', 'mpg'],
-    ['cyl', 'cyl'],
-    ['disp', 'disp'],
-    ['hp', 'hp'],
-    ['drat', 'drat'],
-    ['wt', 'wt'],
-    ['qsec', 'qsec'],
-    ['vs', 'vs'],
-    ['am', 'am'],
-    ['gear', 'gear'],
-    ['carb', 'carb']
-  ],
-  airquality: [
-    ['Ozone', 'Ozone'],
-    ['Solar.R', 'Solar.R'],
-    ['Wind', 'Wind'],
-    ['Temp', 'Temp'],
-    ['Month', 'Month'],
-    ['Day', 'Day']
-  ],
-  meuse: [
-    ['x', 'x'],
-    ['y', 'y'],
-    ['cadmium', 'cadmium'],
-    ['copper', 'copper'],
-    ['lead', 'lead'],
-    ['zinc', 'zinc'],
-    ['elev', 'elev'],
-    ['dist', 'dist'],
-    ['om', 'om'],
-    ['ffreq', 'ffreq'],
-    ['soil', 'soil'],
-    ['lime', 'lime'],
-    ['landuse', 'landuse'],
-    ['dist.m', 'dist.m']
-  ]
-};
-
-
-// --- Data Loading Blocks ---
 // Blocks to load various types of data sources into the workspace
   
 Blockly.defineBlocksWithJsonArray([
